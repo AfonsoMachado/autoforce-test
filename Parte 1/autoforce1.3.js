d2db98d8d7e8386d27ce2b7874515fa5909cdf88
Afonso Machado <afonsosmachado@gmail.com>
2022-11-22 19:14:42 -0300

    adicionado desafio 1.3

---
+++ Parte 1/autoforce1.3.js
@@ -0,0 +1,14 @@
+const whatsButtons = document.querySelectorAll(".button--whatsapp");
+const whatsUrl = "https://api.whatsapp.com/send?phone=5583988971010";
+
+for (const whatsButton of whatsButtons) {
+  recreateNode(whatsButton);
+  whatsButton.setAttribute("onclick", `window.open("${whatsUrl}", "_blank")`);
+}
+
+/** Função para remover todos os eventos de um Node */
+function recreateNode(el) {
+  var newEl = el.cloneNode(false);
+  while (el.hasChildNodes()) newEl.appendChild(el.firstChild);
+  el.parentNode.replaceChild(newEl, el);
+}
