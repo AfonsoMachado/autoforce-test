660b4274e4840aaa2876505eb690bd0117ce6143
Afonso Machado <afonsosmachado@gmail.com>
2022-11-23 01:06:39 -0300

    add cabrio description

--- autoforce-dealer/src/pages/Autoforce/index.jsx
+++ autoforce-dealer/src/pages/Autoforce/index.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import Cabrio from '../../components/Cabrio'
-import Header from '../../components/Header'
-import NavMenu from '../../components/NavMenu'
-import './index.scss'
+import React, { Component } from "react";
+import Cabrio from "../../components/Cabrio";
+import CabrioDescription from "../../components/CabrioDescription";
+import Header from "../../components/Header";
+import NavMenu from "../../components/NavMenu";
+import "./index.scss";
 
 export default class Autoforce extends Component {
   render() {
     return (
-      <div className='app-wrapper'>
-        <Header/>
-        <NavMenu/>
-        <Cabrio/>
+      <div className="app-wrapper">
+        <Header />
+        <NavMenu />
+        <Cabrio />
+        <CabrioDescription />
       </div>
-    )
+    );
   }
 }
